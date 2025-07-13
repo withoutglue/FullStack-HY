334756fa24d321c0129e3f92bc877ab870f0c98f
Simo Liimatta <simo.liimatta@helsinki.fi>
2025-07-13 22:25:06 +0300

    1.2: kurssitiedot step2. Added another component, Part, so that Content is cleaner.

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -13,19 +13,23 @@ const Header = (props) => {
   )
 }
 
+const Part = (props) => {
+  return (
+    <div>
+      <p>
+        {props.part} {props.exercise}
+      </p>
+    </div>
+  )
+}
+
 const Content = (props) => {
   console.log(props)
   return (
     <div>
-      <p>
-        {props.part1} {props.exercise1}
-      </p>
-      <p>
-        {props.part2} {props.exercise2}
-      </p>
-      <p>
-        {props.part3} {props.exercise3}
-      </p>
+      <Part part={props.part1} exercise={props.exercise1}/>
+      <Part part={props.part2} exercise={props.exercise2}/>
+      <Part part={props.part3} exercise={props.exercise3}/>
     </div>
   )
 }
